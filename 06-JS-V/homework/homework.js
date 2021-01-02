839396ae8099f2cc0f9ff5838b2575c8b3c11b3e
Francisco Granja <franbbr110@gmail.com>
2021-01-02 16:32:35 -0300

    HW 5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,15 +9,15 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
   function Usuario(opciones) {
-    this.usuario = opciones.usuario;
-    this.nombre = opciones.nombre;
-    this.email = opciones.email;
-    this.password = opciones.password;
+  this.usuario = opciones.usuario;
+  this.nombre = opciones.nombre;
+  this.email = opciones.email;
+  this.password = opciones.password;
   }
   Usuario.prototype.saludar = function() {
-    return 'Hola, mi nombre es ' + this.nombre;
+  return 'Hola, mi nombre es ' + this.nombre;
   }
-  return Usuario;
+return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
